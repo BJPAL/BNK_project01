dbad678f68b761936459e05cc05725922575b930
BJPAL <jackkim95@naver.com>
2025-07-08 11:37:54 +0900

    1st
    start project

---
+++ src/main/java/com/example/fund/BnkProject01Application.java
@@ -0,0 +1,13 @@
+package com.example.fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnkProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnkProject01Application.class, args);
+	}
+
+}
