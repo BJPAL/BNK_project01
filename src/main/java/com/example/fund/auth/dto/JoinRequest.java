97698d82a0b82dbcc3c7260a00e3f4463b1da2f1
koojeongmin <gjm9834@naver.com>
2025-07-10 14:01:39 +0900

    하이픈 추가 세션만료 수정완료

--- src/main/java/com/example/fund/auth/dto/JoinRequest.java
+++ src/main/java/com/example/fund/auth/dto/JoinRequest.java
@@ -21,7 +21,7 @@ public class JoinRequest {
     @Pattern(regexp = "^[가-힣]{2,4}$")
     private String name;
 
-    // ✅ 하이픈 포함된 전화번호 허용 (예: 010-1234-5678)
+
     @Pattern(
             regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$",
             message = "전화번호 형식이 올바르지 않습니다. 예: 010-1234-5678"
