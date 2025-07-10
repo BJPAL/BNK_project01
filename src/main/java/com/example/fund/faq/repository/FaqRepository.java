517606a8c5a1be76a5cda02f0dc3a8baaac1deaf
ynzzi <syg7474@naver.com>
2025-07-10 15:12:12 +0900

    사용자 FAQ, QnA 추가
    사용자 화면 FAQ 추가
    사용자 화면 QnA 추가 -> 로그인 연동 X, merge 후 수정 예정

--- src/main/java/com/example/fund/faq/repository/FaqRepository.java
+++ src/main/java/com/example/fund/faq/repository/FaqRepository.java
@@ -1,9 +1,14 @@
 package com.example.fund.faq.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.fund.faq.entity.Faq;
 
 public interface FaqRepository extends JpaRepository<Faq, Integer> {
+	
+	// 제목 또는 답변에 키워드가 포함된 FAQ 검색
+	Page<Faq> findByQuestionContainingOrAnswerContaining(String keyword1, String keyword2, Pageable pageable);
 
 }
