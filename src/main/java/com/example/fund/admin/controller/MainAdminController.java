3db836a83b002cbe9912c5f94185fe0e004c35d4
sohee <son5839@naver.com>
2025-07-11 16:37:55 +0900

    250711작업내용
    관리자 설명 메뉴 작업 완료(css제외)

--- src/main/java/com/example/fund/admin/controller/MainAdminController.java
+++ src/main/java/com/example/fund/admin/controller/MainAdminController.java
@@ -9,11 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -27,16 +28,22 @@ public class MainAdminController {
         return "admin/login";
     }
 
+    @GetMapping("/main")
+    public String main(){
+        return "admin/main";
+    }
+
     @PostMapping("/login")
     public String login(AdminDTO adminDTO, HttpServletRequest request, RedirectAttributes rttr){
         HttpSession session = request.getSession();
 
         if(adminService_a.login(adminDTO)){
             Admin admin = adminService_a.searchAdmin(adminDTO);
-            AdminDTO sessionAdmin = new AdminDTO(); //세션등록을 위한 DTO (role, name, admin_Id만 세션에 저장)
+            AdminDTO sessionAdmin = new AdminDTO(); //세션등록을 위한 DTO (role, name, admin_Id, adminname만 세션에 저장)
             sessionAdmin.setRole(admin.getRole());
             sessionAdmin.setAdmin_id(admin.getAdmin_id());
             sessionAdmin.setName(admin.getName());
+            sessionAdmin.setAdminname(admin.getAdminname());
 
             session.setAttribute("admin", sessionAdmin); //sessionAdmin으로 session등록
             return "admin/main";
@@ -64,11 +71,56 @@ public class MainAdminController {
         return "admin/super/admin_register";
     }
 
-    @PostMapping("/adminRegist") //관리자 등록 처리
+    @GetMapping("/adminSetting")
+    public String adminSetting(){
+        return "admin/super/adminSetting";
+    }
+
+    //관리자 등록 처리
+    @PostMapping("/adminRegist")
     public String adminRegist(AdminDTO adminDTO, RedirectAttributes rttr){
         adminService_a.adminRegist(adminDTO);
         String msg = "관리자 등록을 완료하였습니다.";
         rttr.addFlashAttribute("msg", msg);
-        return "admin/main";
+        return "admin/super/adminSetting";
+    }
+
+    //관리자 리스트 컨트롤러(role 파라미터는 필수값 X)
+    @GetMapping("/list")
+    public String getAdminList(@RequestParam(required = false) String role, Model model){
+        List<AdminDTO> admins = new ArrayList<>();
+        if (role == null || role.isEmpty()) {
+            admins = adminService_a.getAllAdmins();
+        } else {
+            admins = adminService_a.getAdminsByRole(role); // 역할별 조회
+        }
+        model.addAttribute("adminList", admins);
+        return "admin/super/adminList :: admin-list-content";
+    }
+
+    //관리자 리스트 >> 상세정보 조회
+    @GetMapping("/detail/{id}")
+    public String getAdminDetail(@PathVariable("id")Integer id, Model model){
+        AdminDTO admin = adminService_a.findById(id);
+        model.addAttribute("admin", admin);
+
+        return "admin/super/adminList :: admin-modify-modal";
+    }
+
+    //관리자 리스트 >> 상세정보 조회 >> 수정작업
+    @PostMapping("/updateRole")
+    @ResponseBody
+    public String updateRole(@RequestBody AdminDTO adminDTO){
+        System.out.println(adminDTO);
+        adminService_a.updateRole(adminDTO.getAdmin_id(), adminDTO.getRole());
+        return "success";
+    }
+
+    //관리자 리스트 >> 상세정보 조회 >> 삭제작업
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public String deleteAdmin(@PathVariable("id")Integer id){
+        adminService_a.deleteAdmin(id);
+        return "success";
     }
 }
