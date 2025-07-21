4b90f99598447a670522ad357cc984d592e6d9e8
ynzzi <syg7474@naver.com>
2025-07-21 11:37:38 +0900

    MBTI 전면 교체
    fundMBTI.html
    -> test.html로 교체
    혹시몰라서 fundMBTI.html 삭제 X

---
+++ src/main/java/com/example/fund/fund/controller/FundMainController.java
@@ -0,0 +1,13 @@
+package com.example.fund.fund.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FundMainController {
+	
+	@GetMapping("/fundMain")
+	public String fundMain() {
+		return "fundMain";
+	}
+}
