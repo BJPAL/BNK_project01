54a5b86ec806d17d2848b0258d25b1253b79a330
BJPAL <jackkim95@naver.com>
2025-07-23 09:52:03 +0900

    0723
    header 수정 commit

--- src/main/java/com/example/fund/auth/controller/MainController.java
+++ src/main/java/com/example/fund/auth/controller/MainController.java
@@ -29,11 +29,6 @@ public class MainController {
             }
         }
 
-        return "main";
-    }
-
-    @GetMapping("/user/")
-    public String userPage(){
         return "user/index";
     }
 }
